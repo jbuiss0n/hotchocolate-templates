b6c2e008bf8d5ebcc64f6a78f1b91ac562bccd25
Michael Staib <michael.staib@hotmail.com>
2018-07-04 15:26:02 +0200

    Reworked build

--- build.ps1
+++ build.ps1
@@ -1,2 +1,18 @@
+if (!!$env:APPVEYOR_REPO_TAG_NAME) {
+    $version = $env:APPVEYOR_REPO_TAG_NAME
+}
+elseif (!!$env:APPVEYOR_BUILD_VERSION) {
+    $version = $env:APPVEYOR_BUILD_VERSION
+}
+
+if ($version -ne $null) {
+    $env:Version = $version
+}
+
+$nuspecs = ls *.nuspec -Recurse
+foreach ($nuspec in $nuspecs) {
+    (Get-Content $nuspec.FullName).replace('<version>0.0.0</version>', '<version>' + $version + '</version>') | Set-Content $nuspec.FullName
+}
+
 nuget.exe pack StarWars\HotChocolate.Templates.StarWars.nuspec
 nuget.exe pack Server\HotChocolate.Templates.Server.nuspec
